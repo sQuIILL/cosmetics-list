2b0c264c9e3ba6a1bb0ce7462a123515b8c44d51
Sebastian Piekarczyk <sebastian.piekarczyk@azurro.pl>
2022-12-06 00:04:45 +0100

    deleting console.log

--- src/components/ProductModal/ProductModal.jsx
+++ src/components/ProductModal/ProductModal.jsx
@@ -24,8 +24,8 @@ export default function ProductModal({ productInfo:
 
     return (
         <>
-            <div style={{ position: "fixed", top: "0", height: "100%", left: "0", right: "0", margin: "auto", backgroundColor: "grey", width: "100%", opacity: "0.7" }} onClick={closeModal} />
-            <div key={id} style={{ position: "fixed", top: "0", bottom: "0", height: "550px", left: "0", right: "0", margin: "auto", backgroundColor: "white", width: "750px", borderRadius: '50px', boxShadow: "1px 2px 20px 1px " }}>
+            <div style={{ position: "fixed", top: "0", height: "100%", left: "0", right: "0", margin: "auto", backgroundColor: "grey", width: "100%", opacity: "0.6" }} onClick={closeModal} />
+            <div key={id} style={{ position: "fixed", top: "0", bottom: "0", height: "550px", left: "0", right: "0", margin: "auto", backgroundColor: "white", width: "1050px", borderRadius: '50px', boxShadow: "1px 2px 20px 1px " }}>
                 <div className='productModalContainer'>
                     <div style={{ fontSize: "32px", fontWeight: "500", marginBottom: "15px" }}>
                         {name}
@@ -41,7 +41,7 @@ export default function ProductModal({ productInfo:
                             />
                         </div>
                         <div style={{ display: "flex", flexDirection: "column", }}>
-                            <div style={{ width: "100%", display: "flex", justifyContent: "space-evenly", }}>
+                            <div style={{  display: "flex", justifyContent: "space-evenly", }}>
                                 <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", gap: "10px", fontSize: '20px' }}>
                                     <div>Brand:</div>
                                     <div>Category: &nbsp;</div>
